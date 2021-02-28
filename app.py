e536d87db0bc0d9953922d50dc2eeffa69446526
berrymatthewj <72576569+berrymatthewj@users.noreply.github.com>
2021-02-28 14:55:06 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,8 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
 
 # reflect an existing database into a new model
 
@@ -23,8 +24,8 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #save reference to the table
-measurement = Base.classes.measurement
-session = Session(engine)
+Measurement = Base.classes.measurement
+
 #################################################
 # Flask Setup
 #################################################
@@ -40,20 +41,58 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/dates<br/>"
-        f"/api/v1.0/undefined"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start>/<end>"
     )
 
-@app.route("/api/v1.0/dates")
-def dates():
-    results = session.query(measurement.date).all()
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.date,Measurement.prcp).filter(Measurement.date >= query_date).order_by(Measurement.date.desc()).all()
     session.close()
+
+    # Create a dictionary from the row data and append to a list
+    precip_data = []
+    for date, prcp in results:
+        precip_dict = {}
+        precip_dict["date"] = date
+        precip_dict["prcp"] = prcp
+        precip_data.append(precip_dict)
+    
+    return jsonify(precip_data)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    results = session.query(Measurement.station).distinct().all()
+    session.close()
+
     return jsonify(results)
-    # Convert list of tuples into normal list
-#     # all_dates = list(np.ravel(results))
 
-#     # return jsonify(all_dates)
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    query_date = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).filter(Measurement.date >= query_date).filter(Measurement.station=="USC00519281").all()
+    session.close()
 
+    return jsonify(results)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temperature(start=None,end=None):
+    session = Session(engine)
+    if end==None:
+        results = session.query(func.min(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.station=="USC00519281"),func.max(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.station=="USC00519281"),func.avg(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.station=="USC00519281")).all()
+    else:
+        results = session.query(func.min(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.station=="USC00519281"),func.max(Measurement.tobs).filter(Measurement.date >= start).filter(Measurement.station=="USC00519281"),func.avg(Measurement.tobs).filter(Measurement.date >= start,Measurement.date <= end).filter(Measurement.station=="USC00519281")).all()
+    session.close()
+
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
